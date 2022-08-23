1d84de5aea2d10c89a93cb11f5c8eeaf88700102
Xavier Charles Hazzard <xavier@hazzard.org.uk>
2022-08-23 22:37:13 +0100

    Added main functions

--- main.go
+++ main.go
@@ -15,5 +15,6 @@ func main() {
 		})
 	})
 	fmt.Println("Server running on Port 8080")
+	fmt.Println(helpers.split("This is a string/nthis is another string"))
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
