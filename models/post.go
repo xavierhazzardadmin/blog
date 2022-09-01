72efd8f89d5dcb362667ab736a70f3a2da5ad617
Xavier Charles Hazzard <xavier@hazzard.org.uk>
2022-09-01 09:44:18 +0100

    Updated the model struct

--- models/post.go
+++ models/post.go
@@ -5,8 +5,8 @@ type Post struct {
 	Content   string `json:"content"`
 	ID        int    `json:"id"`
 	MetaTitle string `json:"metaTitle"`
-	Published int64  `json:"published"`
+	Published string  `json:"published"`
 	Slug      string `json:"slug"`
 	Title     string `json:"title"`
-	Updated   int64  `json:"updated"`
+	Updated   string  `json:"updated"`
 }
